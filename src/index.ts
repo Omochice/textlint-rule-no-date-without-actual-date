496eeff81692d954016f7402151a6b8c4349a32e
Omochice <mochice.mls.ntl@gmail.com>
2022-09-07 05:51:48 +0900

    :muscle: Add fix rule

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,90 @@
 import type { TextlintRuleModule } from "@textlint/types";
+import { add, Duration, format } from "date-fns";
 
-const detect = (text: string): boolean => {
-  const markers = [
-    "本日",
-    "今日",
-    "明日",
-    "昨日",
-    "今月",
-    "来月",
-    "昨月",
-    "先月",
-    "今週",
-    "来週",
-    "先週",
-    "来年",
-    "今年",
-    "去年",
-  ];
+type Marker = {
+  str: string;
+  format: string;
+  duration?: Duration;
+  convertToWeekStart?: boolean;
+};
+
+type Matched = {
+  index: number;
+  text: string;
+};
+
+// TODO: load markers from other source
+const markers: Marker[] = [
+  { str: "本日", format: "MM/dd" },
+  { str: "今日", format: "MM/dd" },
+  { str: "明日", format: "MM/dd", duration: { days: 1 } },
+  { str: "昨日", format: "MM/dd", duration: { days: -1 } },
+  { str: "今月", format: "M月" },
+  { str: "来月", format: "M月", duration: { months: 1 } },
+  { str: "昨月", format: "M月", duration: { months: -1 } },
+  { str: "先月", format: "M月", duration: { months: -1 } },
+  { str: "今週", format: "MM/dd週", convertToWeekStart: true },
+  {
+    str: "来週",
+    format: "MM/dd週",
+    convertToWeekStart: true,
+    duration: { weeks: 1 },
+  },
+  {
+    str: "先週",
+    format: "MM/dd週",
+    convertToWeekStart: true,
+    duration: { weeks: -1 },
+  },
+  { str: "今年", format: "YYYY年" },
+  { str: "来年", format: "YYYY年", duration: { years: 1 } },
+  { str: "去年", format: "YYYY年", duration: { years: -1 } },
+];
+
+const fix = (marker: string): string => {
+  const rule = markers.find((e) => e.str === marker);
+  if (rule === undefined) {
+    throw new Error("???????");
+  }
+  const today = new Date();
+  const delta = rule.convertToWeekStart ? Number(format(today, "i")) - 1 : 0;
+  const targetDate = add(today, { days: -delta });
+  const formatted = format(add(targetDate, rule.duration ?? {}), rule.format);
+  return `${marker}(${formatted})`;
+};
+
+const detect = (text: string) => {
   for (const marker of markers) {
-    const re = new RegExp(`${marker}[^\(（].*[^\)）]`);
-    if (re.test(text)) {
-      return true;
+    const re = new RegExp(`(${marker.str})[^\(（].*[^\)）]`);
+    const matched = re.exec(text);
+    if (matched) {
+      const processed: Matched = { text: matched[1], index: matched.index };
+      return processed;
     }
   }
-  return false;
+  return null;
 };
 
 const reporter: TextlintRuleModule = (context) => {
-  const { getSource, RuleError, Syntax, report } = context;
+  const { getSource, RuleError, Syntax, report, fixer } = context;
 
   return {
-    [Syntax.Str](node) {
+    async [Syntax.Str](node) {
       const text = getSource(node);
-      if (detect(text)) {
+      const matched = detect(text);
+      if (matched) {
         report(
           node,
-          new RuleError(`${text} must has actual date within.`),
+          new RuleError(`${matched.text} must has actual date within.`, {
+            fix: fixer.replaceTextRange([
+              matched.index,
+              matched.index + matched.text.length,
+            ], fix(matched.text)),
+          }),
         );
       }
     },
-    // [Syntax.ListItem](node) {
-    //   const text = getSource(node);
-    //   console.log(text)
-    //   if (detect(text)) {
-    //     report(
-    //       node,
-    //       new RuleError(`${text} must has actual date within.`),
-    //     );
-    //   }
-    // },
   };
 };
 
-export default reporter;
+export default { linter: reporter, fixer: reporter };
