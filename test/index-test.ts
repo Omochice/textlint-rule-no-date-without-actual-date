e6de3e9ca04266eacb1ba6e55ad1ed85ee799bde
Omochice <mochice.mls.ntl@gmail.com>
2022-09-09 03:00:45 +0900

    :herb: Add normal tests

--- test/index-test.ts
+++ test/index-test.ts
@@ -2,9 +2,44 @@ import TextLintTester from "textlint-tester";
 import rule from "../src/index";
 const tester = new TextLintTester();
 
-tester.run("If text is expected format, should pass", rule, {
+tester.run("If no text is checked, should pass", rule, {
+  valid: [{
+    text: "",
+    options: { lang: "ja", markers: [] },
+  }],
+});
+
+tester.run("If text is normal, should pass", rule, {
+  valid: [{
+    text: "吾輩は猫である。名前はまだない。",
+    options: { lang: "ja", markers: [{ str: "今日", format: "MM/dd" }] },
+  }],
+});
+
+tester.run("If text is bracketed with harf-width bracket, should pass", rule, {
   valid: [{
     text: "今日(09/09)対応予定",
+    options: { lang: "ja", markers: [{ str: "今日", format: "MM/dd" }] },
+  }],
+});
+
+tester.run("If text is bracketed with full-width bracket, should pass", rule, {
+  valid: [{
+    text: "今日(09/09)対応予定",
+    options: { lang: "ja", markers: [{ str: "今日", format: "MM/dd" }] },
+  }],
+});
+
+tester.run("If text is unmatched with rule, should pass", rule, {
+  valid: [{
+    text: "今日対応予定",
+    options: { lang: "ja", markers: [{ str: "明日", format: "MM/dd" }] },
+  }],
+});
+
+tester.run("If text is matched but it is not date text, should pass", rule, {
+  valid: [{
+    text: "今日子さん",
     options: { lang: "ja", markers: [{ str: "今日", format: "MM/dd" }] },
   }],
 });
