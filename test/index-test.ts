d0d55282120479ea9911e4d696d7d183c4d72189
Omochice <mochice.mls.ntl@gmail.com>
2022-09-09 01:38:23 +0900

    :herb: Add sample test code

--- test/index-test.ts
+++ test/index-test.ts
@@ -1,48 +1,10 @@
 import TextLintTester from "textlint-tester";
 import rule from "../src/index";
 const tester = new TextLintTester();
-// ruleName, rule, { valid, invalid }
-tester.run("rule", rule, {
-    valid: [
-        // no problem
-        "text",
-        {
-            text: "It is bugs, but it should be ignored",
-            options: {
-                allows: ["it should be ignored"]
-            }
-        }
-    ],
-    invalid: [
-        // single match
-        {
-            text: "It is bugs.",
-            errors: [
-                {
-                    message: "Found bugs.",
-                    line: 1,
-                    column: 7
-                }
-            ]
-        },
-        // multiple match
-        {
-            text: `It has many bugs.
 
-One more bugs`,
-            errors: [
-                {
-                    message: "Found bugs.",
-                    line: 1,
-                    column: 13
-                },
-                {
-                    message: "Found bugs.",
-                    line: 3,
-                    column: 10
-                }
-            ]
-        },
-
-    ]
+tester.run("If text is expected format, should pass", rule, {
+  valid: [{
+    text: "今日(09/09)対応予定",
+    options: { lang: "ja", markers: [{ str: "今日", format: "MM/dd" }] },
+  }],
 });
